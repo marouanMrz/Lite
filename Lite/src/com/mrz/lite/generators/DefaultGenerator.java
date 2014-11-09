50dbdef43c3ac7a1aa2682960b2afbaff2f41329
marouanMrz <marouan.mrz@gmail.com>
2014-11-09 03:03:00 +0000

    Add DefaultGenerator implementation

---
+++ Lite/src/com/mrz/lite/generators/DefaultGenerator.java
@@ -0,0 +1,13 @@
+package com.mrz.lite.generators;
+
+import javax.tools.JavaFileObject;
+
+public class DefaultGenerator implements Generator {
+
+	@Override
+	public void generate(JavaFileObject jfo, String className, String packageName,
+			String fullQualifiedClassName) {
+		
+	}
+
+}
