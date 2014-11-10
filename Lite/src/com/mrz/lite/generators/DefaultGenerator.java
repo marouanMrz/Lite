56d0c5710dfa5d165aacc25f1acb359991c9c67b
marouanMrz <marouan.mrz@gmail.com>
2014-11-10 18:50:39 +0000

    Update the generator signature to use EntityModel

--- Lite/src/com/mrz/lite/generators/DefaultGenerator.java
+++ Lite/src/com/mrz/lite/generators/DefaultGenerator.java
@@ -2,11 +2,12 @@ package com.mrz.lite.generators;
 
 import javax.tools.JavaFileObject;
 
+import com.mrz.lite.models.EntityModel;
+
 public class DefaultGenerator implements Generator {
 
 	@Override
-	public void generate(JavaFileObject jfo, String className, String packageName,
-			String fullQualifiedClassName) {
+	public void generate(JavaFileObject jfo, EntityModel entityModel) {
 		
 	}
 
--- Lite/src/com/mrz/lite/generators/Generator.java
+++ Lite/src/com/mrz/lite/generators/Generator.java
@@ -2,6 +2,8 @@ package com.mrz.lite.generators;
 
 import javax.tools.JavaFileObject;
 
+import com.mrz.lite.models.EntityModel;
+
 public interface Generator {
-	public void generate(JavaFileObject jfo, String className, String packageName, String fullQualifiedClassName);
+	public void generate(JavaFileObject jfo, EntityModel entityModel);
 }
