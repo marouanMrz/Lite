cf8b45364250e39067a9e84facb2ddb4f77e70ac
marouanMrz <marouan.mrz@gmail.com>
2014-11-15 01:17:53 +0000

    Retrieve fields(name & type) to generate the source code

--- Lite/src/com/mrz/lite/processors/LiteProcessor.java
+++ Lite/src/com/mrz/lite/processors/LiteProcessor.java
@@ -1,7 +1,6 @@
 package com.mrz.lite.processors;
 
 import java.io.IOException;
-import java.lang.reflect.Field;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
@@ -15,6 +14,9 @@ import javax.lang.model.element.Element;
 import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.PackageElement;
 import javax.lang.model.element.TypeElement;
+import javax.lang.model.element.VariableElement;
+import javax.lang.model.type.TypeKind;
+import javax.lang.model.util.ElementFilter;
 import javax.tools.JavaFileObject;
 
 import com.mrz.lite.annotations.LiteEntity;
@@ -36,30 +38,16 @@ public class LiteProcessor extends AbstractProcessor {
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment roundEnv) {
 		Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(LiteEntity.class);
+//		Messager m = this.processingEnv.getMessager();
 		for (Element e : elements) {
 			if (e.getKind() == ElementKind.CLASS) {
 				entityModel = new EntityModel();
 				TypeElement classElement = (TypeElement) e;
 				PackageElement packageElement = (PackageElement) classElement.getEnclosingElement();
-				
 				entityModel.setPackageName(packageElement.getQualifiedName().toString());
 				entityModel.setFullQualifiedClassName(classElement.getQualifiedName().toString());
 				entityModel.setClassName(classElement.getSimpleName().toString());
-				
-				Class<?> clazz = null;
-				try {
-					clazz = Class.forName(entityModel.getClassName());
-				} catch (ClassNotFoundException e1) {
-					e1.printStackTrace();
-				}
-				List<FieldModel> fields = new LinkedList<FieldModel>();
-				for (Field field : clazz.getDeclaredFields()) {
-					FieldModel fieldModel = new FieldModel();
-					fieldModel.setName(field.getName());
-					fieldModel.setType(field.getType());
-					fields.add(fieldModel);
-				}
-				entityModel.setFields(fields);
+				entityModel.setFields(fieldsMapper(classElement));
 			}
 		}
 		JavaFileObject jfo = null;
@@ -72,5 +60,35 @@ public class LiteProcessor extends AbstractProcessor {
 		generator.generate(jfo, entityModel);
 		return true;
 	}
-
+	
+	/**
+	 * Return the simple name of field type
+	 * @param fieldType qualified name of type
+	 * @return simple name of type
+	 */
+	private String formatFieldType(String fieldType) {
+		String[] type = fieldType.split("\\.");
+		return type[type.length - 1];
+	}
+	
+	/**
+	 * Retrieve typeElement fields to construct a list of FieldModel 
+	 * @param classElement TypeElement of kind CLASS
+	 * @return List of FieldModel
+	 */
+	private List<FieldModel> fieldsMapper(TypeElement classElement){
+		List<FieldModel> fields = new LinkedList<FieldModel>();
+		List<VariableElement> variableElements = ElementFilter.fieldsIn(classElement.getEnclosedElements());
+		for (VariableElement field : variableElements) {
+			FieldModel fieldModel = new FieldModel();
+			fieldModel.setName(field.getSimpleName().toString());
+			if (field.asType().getKind() == TypeKind.DECLARED) {
+				fieldModel.setType(formatFieldType(field.asType().toString()));
+			} else {
+				fieldModel.setType(field.asType().toString());
+			}
+			fields.add(fieldModel);
+		}
+		return fields;
+	}
 }
