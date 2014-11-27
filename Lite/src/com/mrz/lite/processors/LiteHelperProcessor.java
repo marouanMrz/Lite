0fd3dcbd0eb12497493a9c15331a40ba4d7dfba7
marouanMrz <marouan.mrz@gmail.com>
2014-11-27 22:29:36 +0000

    Process the helper class and store sql statement in SqlStatementHelper class

--- Lite/src/com/mrz/lite/processors/LiteHelperProcessor.java
+++ Lite/src/com/mrz/lite/processors/LiteHelperProcessor.java
@@ -1,18 +1,44 @@
 package com.mrz.lite.processors;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 import javax.annotation.processing.AbstractProcessor;
 import javax.annotation.processing.RoundEnvironment;
+import javax.annotation.processing.SupportedAnnotationTypes;
+import javax.annotation.processing.SupportedSourceVersion;
+import javax.lang.model.SourceVersion;
+import javax.lang.model.element.Element;
+import javax.lang.model.element.ElementKind;
 import javax.lang.model.element.TypeElement;
 
-public class LiteHelperProcessor extends AbstractProcessor {
+import com.mrz.lite.annotations.LiteHelper;
 
+@SupportedAnnotationTypes({"com.mrz.lite.annotations.LiteHelper"})
+@SupportedSourceVersion(SourceVersion.RELEASE_7)
+public class LiteHelperProcessor extends AbstractProcessor {
+	
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations,
 			RoundEnvironment roundEnv) {
-		// TODO Auto-generated method stub
-		return false;
+		Set<? extends Element> elements = roundEnv.getElementsAnnotatedWith(LiteHelper.class);
+		for (Element e : elements) {
+			if (e.getKind() == ElementKind.CLASS) {
+				TypeElement classElement = (TypeElement) e;
+				HashMap<String, String> fieldsValues = ProcessorHelper.getFieldsValues(classElement);
+				HashMap<String, String> sqlCreate = new HashMap<String, String>();
+				HashMap<String, String> sqlDelete = new HashMap<String, String>();
+				for(Map.Entry<String, String> fieldsValuesEntry : fieldsValues.entrySet()){
+					if (fieldsValuesEntry.getKey().toLowerCase().contains("create")) {
+						sqlCreate.put(fieldsValuesEntry.getKey(), fieldsValuesEntry.getValue());
+					} else {
+						sqlDelete.put(fieldsValuesEntry.getKey(), fieldsValuesEntry.getValue());
+					}
+				}
+			}
+		}
+		return true;
 	}
 
 }
