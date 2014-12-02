ee867da2ec4c4105e7efe4fd5b0e5e32c17ee7e7
marouanMrz <marouan.mrz@gmail.com>
2014-12-02 15:50:13 +0000

    Implement SQLiteOpenHelper methods & Update packages import

--- Lite/src/com/mrz/lite/generators/DaoGenerator.java
+++ Lite/src/com/mrz/lite/generators/DaoGenerator.java
@@ -1,7 +1,6 @@
 package com.mrz.lite.generators;
 
 import java.io.BufferedWriter;
-import java.util.Hashtable;
 
 import javax.tools.JavaFileObject;
 
@@ -39,7 +38,7 @@ public class DaoGenerator implements Generator {
 			bw.newLine();
 			bw.append(" * The LiteDao<T> generic class allows you to do CRUD");
 			bw.newLine();
-			bw.append(" * operations on a specefic entity.");
+			bw.append(" * operations on a specific entity.");
 			bw.newLine();
 			bw.append(" * ");
 			bw.newLine();
@@ -58,7 +57,13 @@ public class DaoGenerator implements Generator {
 			bw.newLine();
 			bw.append("import android.os.Build;");
 			bw.newLine();
-			bw.append("import com.mrz.lite.core;");
+			bw.append("import com.mrz.lite.core.DaoHelper;");
+			bw.newLine();
+			bw.append("import java.util.Hashtable;");
+			bw.newLine();
+			bw.append("import java.util.LinkedList;");
+			bw.newLine();
+			bw.append("import java.util.Map;");
 			bw.newLine();
 			bw.newLine();
 			bw.append("public class LiteDao<T> extends SQLiteOpenHelper {");
@@ -77,6 +82,8 @@ public class DaoGenerator implements Generator {
 			bw.newLine();
 			bw.append("    public static final String DATABASE_NAME = \"LiteEmbedded.db\";");
 			bw.newLine();
+			bw.newLine();
+			bw.newLine();
 			bw.append("    public LiteDao(Context context, Object setAlwaysThis) {");
 			bw.newLine();
 			bw.append("        super(context, DATABASE_NAME, null, DATABASE_VERSION);");
@@ -91,13 +98,48 @@ public class DaoGenerator implements Generator {
 			bw.newLine();
 			bw.append("        sqlStatements = DaoHelper.getStatements(this.uri, DaoHelper.format(setAlwaysThis.getClass().getCanonicalName()));");
 			bw.newLine();
-			bw.append("        sqlCreate = DaoHelper.getCreateStatement(sqlStatements);");
+			bw.append("        sqlCreate = DaoHelper.getCreateStatements(sqlStatements);");
 			bw.newLine();
-			bw.append("        sqlDelete = DaoHelper.getDeleteStatement(sqlStatements);");
+			bw.append("        sqlDelete = DaoHelper.getDeleteStatements(sqlStatements);");
 			bw.newLine();
 			bw.append("    }");
-			// Implement SQLiteOpenHelper methods
-			
+			bw.newLine();
+			bw.newLine();
+			bw.append("    public void onCreate(SQLiteDatabase db) {");
+			bw.newLine();
+			bw.append("        for(Map.Entry<String, String> createEntry : sqlCreate.entrySet()) {");
+			bw.newLine();
+			bw.append("            db.execSQL(createEntry.getValue());");
+			bw.newLine();
+			bw.append("        }");
+			bw.newLine();
+			bw.append("    }");
+			bw.newLine();
+			bw.newLine();
+			bw.append("    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {");
+			bw.newLine();
+			bw.append("        for(Map.Entry<String, String> deleteEntry : sqlDelete.entrySet()) {");
+			bw.newLine();
+			bw.append("            db.execSQL(deleteEntry.getValue());");
+			bw.newLine();
+			bw.append("        }");
+			bw.newLine();
+			bw.append("        onCreate(db);");
+			bw.newLine();
+			bw.append("    }");
+			bw.newLine();
+			bw.newLine();
+			bw.append("    @TargetApi(Build.VERSION_CODES.HONEYCOMB)");
+			bw.newLine();
+			bw.append("    public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {");
+			bw.newLine();
+			bw.append("        super.onDowngrade(db, oldVersion, newVersion);");
+			bw.newLine();
+			bw.append("    }");
+			bw.newLine();
+			bw.newLine();
+			bw.append("}");
+			bw.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
