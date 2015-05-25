e0ab80de1d165739745b7d14660edf9ca2179c0b
marouanMrz <marouan.mrz@gmail.com>
2015-05-25 23:40:49 +0100

    Rename the OneToMany annotation to ManyToOne

--- Lite/src/com/mrz/lite/annotations/LiteManyToOne.java
+++ Lite/src/com/mrz/lite/annotations/LiteManyToOne.java
@@ -8,6 +8,6 @@ import java.lang.annotation.Target;
 
 @Retention(RetentionPolicy.SOURCE)
 @Target(ElementType.FIELD)
-public @interface LiteOneToMany {
-	Class<?> mappedBy();
+public @interface LiteManyToOne {
+	String mappedBy();
 }
