fdca0927870dea1cc227f691b99a3c7db83bbfb7
marouanMrz <marouan.mrz@gmail.com>
2015-06-21 19:05:46 +0000

    Change retention policy to Runtime instead of Source

--- Lite/src/com/mrz/lite/annotations/LiteManyToOne.java
+++ Lite/src/com/mrz/lite/annotations/LiteManyToOne.java
@@ -6,7 +6,7 @@ import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
 
-@Retention(RetentionPolicy.SOURCE)
+@Retention(RetentionPolicy.RUNTIME)
 @Target(ElementType.FIELD)
 public @interface LiteManyToOne {
 	String mappedBy();
